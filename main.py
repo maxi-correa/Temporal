1c6fccb1b469b6c139602994477f406076856674
maxi-correa <maxis.c.22@gmail.com>
2023-10-16 21:27:39 -0300

    Juego corriendo + menus

--- main.py
+++ main.py
@@ -4,23 +4,21 @@ from constantes import *
 from boton import *
 import sys
 
-
-
 class Juego:
     def __init__(self):
         pygame.init() #inicia pygame
         self.screen = pygame.display.set_mode((PANTALLA_ANCHO, PANTALLA_ALTO))
         pygame.display.set_caption("Adventure Time")
         self.clock = pygame.time.Clock() #Configuracion de FPS
-        #self.fuente = pygame.font.Font('Arial.ttf', 32)
         self.corriendo = True
+        self.tema_batalla_encendido = False
         
-        self.plantilla_jugador = Plantilla_Sprites('img/character.png')
-        self.plantilla_terreno = Plantilla_Sprites('img/terrain.png')
-        self.plantilla_enemigo = Plantilla_Sprites('img/enemy.png')
-        self.plantilla_ataque = Plantilla_Sprites('img/attack.png')
-        self.intro_fondo = pygame.image.load('img/introbackground.png')
-        self.muerte_fondo = pygame.image.load('img/gameover.png')
+        self.plantilla_jugador = Plantilla_Sprites('imagenes/character.png')
+        self.plantilla_terreno = Plantilla_Sprites('imagenes/terrain.png')
+        self.plantilla_enemigo = Plantilla_Sprites('imagenes/enemy.png')
+        self.plantilla_ataque = Plantilla_Sprites('imagenes/attack.png')
+        self.intro_fondo = pygame.image.load('imagenes/introbackground.png')
+        self.muerte_fondo = pygame.image.load('imagenes/gameover.png')
         
         #Carga de imagenes para menú
         self.titulo_img = pygame.image.load('imagenes/adventure_time.png').convert_alpha()
@@ -33,8 +31,15 @@ class Juego:
         
         #Carga de imagen de opción en main
         self.menu_img = pygame.image.load('imagenes/menu_btn.png').convert_alpha()
-
+        
+        #Carga de imagenes de pausa
+        self.back_img = pygame.image.load('imagenes/back_btn.png').convert_alpha()
+        self.option_2_img = pygame.image.load('imagenes/option_2_btn.png').convert_alpha()
+        self.sound_on_2_img = pygame.image.load('imagenes/sound_on_2_btn.png').convert_alpha()
+        self.sound_off_2_img = pygame.image.load('imagenes/sound_off_2_btn.png').convert_alpha()
+        
         #Carga de imagenes para game over
+        self.fondo_game_over_img = pygame.image.load('imagenes/fondo_game_over.png').convert_alpha()
         self.game_over_img = pygame.image.load('imagenes/game_over.png').convert_alpha()
         self.restart_img = pygame.image.load('imagenes/restart_btn.png').convert_alpha()
         self.quit_2_img = pygame.image.load('imagenes/quit_2_btn.png').convert_alpha()
@@ -66,8 +71,6 @@ class Juego:
         self.ataques = pygame.sprite.LayeredUpdates()
         
         self.crear_mapa()
-        
-        self.battle_theme.play(-1)
     
     def eventos(self):
         for evento in pygame.event.get():
@@ -100,22 +103,28 @@ class Juego:
         pygame.display.update()
     
     def main(self):
+        
+        if self.tema_batalla_encendido == False:
+            self.battle_theme.play(-1)
+            self.tema_batalla_encendido = True
+        
         boton_menu = Boton(750, 10, self.menu_img, 1)
         
         #Ciclo de juego
         while self.jugando:
             self.eventos() #Contiene configuración de teclas
             self.actualizar() #Actualiza la pantalla de juego
-            self.mostrar()
+            self.mostrar() #Muestra los Sprites para el juego
             if boton_menu.dibujar(self.screen):
                 self.pausado = True
                 self.pausa()
-        #self.corriendo = False
     
     def pausa(self):
         
         #Instancia de botones
-        boton_resume = Boton(320, 300, self.restart_img, 1.5)
+        boton_back = Boton(320, 300, self.back_img, 1.5)
+        boton_sound_on_2 = Boton(470, 400, self.sound_on_2_img, 1.5)
+        boton_sound_off_2 = Boton(550, 400, self.sound_off_2_img, 1.5)
         boton_quit_2 = Boton(320, 500, self.quit_2_img, 1.5)
         
         #Ciclo de pausa
@@ -128,10 +137,20 @@ class Juego:
             
             self.screen.fill(AZUL)
             self.screen.blit(self.titulo_img, [50,-70])
+            self.screen.blit(pygame.transform.scale(self.option_2_img, (135,55)), [320, 400])
             
-            if boton_resume.dibujar(self.screen):
+            if boton_back.dibujar(self.screen):
                 self.pausado = False
             
+            if boton_sound_on_2.dibujar(self.screen):
+                if self.tema_batalla_encendido == False:
+                    self.battle_theme.play(-1)
+                    self.tema_batalla_encendido = True
+            
+            if boton_sound_off_2.dibujar(self.screen):
+                self.battle_theme.stop()
+                self.tema_batalla_encendido = False
+                
             if boton_quit_2.dibujar(self.screen):
                 self.pausado = False
                 self.jugando = False
@@ -141,27 +160,25 @@ class Juego:
             pygame.display.update()
         
     def game_over(self):
-        
-        #texto = self.fuente.render('Game Over', True, BLANCO)
-        #texto_rect = texto.get_rect(center=(PANTALLA_ANCHO/2, PANTALLA_ALTO/2))
-        
-        #boton_restart = Boton (10, PANTALLA_ALTO - 60 , 120, 50, BLANCO, NEGRO, 'Restart', 32)
 
         for sprite in self.todos_sprites:
-            sprite.kill()
+            sprite.kill() #.kill() es un recurso que elimina todos los sprites de la pantalla
         
         #Instancia de botones
-        boton_resume = Boton(320, 300, self.restart_img, 1.5)
-        boton_quit_2 = Boton(320, 500, self.quit_2_img, 1.5)
+        boton_resume = Boton(100, 320, self.restart_img, 1.5)
+        boton_quit_2 = Boton(350, 320, self.quit_2_img, 1.5)
         
         while self.corriendo:
             for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
                     self.corriendo = False
             
-            self.screen.fill(AZUL)
-            #self.screen.blit(game_over_img, [50,50])
-            self.screen.blit(pygame.transform.scale(self.game_over_img, (600,80)), [100, 100])
+            if self.tema_batalla_encendido:
+                self.battle_theme.stop()
+                self.tema_batalla_encendido = False
+            
+            self.screen.blit(pygame.transform.scale(self.fondo_game_over_img, (960,600)), [-100,0])
+            self.screen.blit(pygame.transform.scale(self.game_over_img, (600,80)), [0, 100])
             
             if boton_resume.dibujar(self.screen):
                 self.nuevo()
@@ -169,16 +186,7 @@ class Juego:
             
             if boton_quit_2.dibujar(self.screen):
                 self.corriendo = False
-            #mouse_pos = pygame.mouse.get_pos()
-            #mouse_presionado = pygame.mouse.get_pressed()
             
-            #if boton_restart.es_presionado(mouse_pos, mouse_presionado):
-            #    self.nuevo()
-            #    self.main()
-            
-            #self.screen.blit(self.muerte_fondo, (0,0))
-            #self.screen.blit(texto, texto_rect)
-            #self.screen.blit(boton_restart.image, boton_restart.rect)
             self.clock.tick(FPS)
             pygame.display.update()
             
@@ -191,7 +199,6 @@ class Juego:
         
         #Instancia de botones
         boton_play = Boton(300, 250, self.play_img, 1)
-        #boton_option = Boton(300, 350, option_img, 1)
         boton_quit = Boton(300, 450, self.quit_img, 1)
         boton_sound_on = Boton (520, 350, self.sound_on_img, 1.4)
         boton_sound_off = Boton(620, 350, self.sound_off_img, 1.4)
@@ -218,11 +225,6 @@ class Juego:
             if boton_play.dibujar(self.screen):
                 intro = False
                 self.menu_theme.stop()
-                #tema_batalla_encendido = True
-                #battle_theme.play(-1)
-            
-            #if boton_option.dibujar(screen):
-            #    print('OPTION')
             
             if boton_sound_on.dibujar(self.screen):
                 if tema_menu_encendido == False:
@@ -245,7 +247,6 @@ juego.intro_screen()
 juego.nuevo()
 while juego.corriendo:
     juego.main()
-    #juego.pausa()
     juego.game_over()
 pygame.quit()
 sys.exit()
